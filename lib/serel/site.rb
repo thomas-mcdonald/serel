b746acf4d4765331aee2cb7edcfb6099945ae0db
Thomas McDonald <tom@conceptcoding.co.uk>
2012-02-29 19:28:45 +0000

    Add support for /sites

---
+++ lib/serel/site.rb
@@ -0,0 +1,22 @@
+module Serel
+  # The Site class represents a Stack Exchange site.
+  #
+  # == Finding Sites
+  #
+  # Sites can be retrieved using +all+ and +get+.
+  #
+  # === all
+  #   Serel::Site.all
+  #
+  # Automatically paginates through the list of sites and returns an array of every Stack Exchange site.
+  #
+  # == get
+  #   Serel::Site.get
+  #
+  # Retrieves a page of sites, applying any scopes that have previously been defined.
+  class Site < Base
+    attributes :site_type, :name, :logo_url, :api_site_parameter, :site_url, :audience, :icon_url, :aliases, :site_state, :styling, :closed_beta_date, :open_beta_date, :launch_date, :favicon_url, :related_sites, :twitter_account, :markdown_extensions
+    finder_methods :all, :get
+    network_wide
+  end
+end
